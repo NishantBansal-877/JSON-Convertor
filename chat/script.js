8e3f65f11f2187a885711c4afe172a1cb37e2c4e
NishantBansal-877 <nishant87706@gmail.com>
2025-07-18 13:48:15 +0530

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -12,7 +12,7 @@ submitBtn.addEventListener("click", function (e) {
   messageBot.insertAdjacentElement("afterend", newUserMessage);
 
   const request = new XMLHttpRequest();
-  request.open("POST", "https://json-convertor-uhqz.onrender.com/user", true);
+  request.open("POST", "http://127.0.0.1:5000/user", true);
   request.setRequestHeader("content-type", "plain/text");
   let responseBoxText = "";
   request.addEventListener("load", function () {
